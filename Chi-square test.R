9c62d64a4deee7078691b60795667af7d600ca47
game-theorist <malpetroni@gmail.com>
2026-08-25 08:06:42 -0300

    Chisquare graphs

--- Chi-square test.R
+++ Chi-square test.R
@@ -17,13 +17,19 @@ stacked_bar_plot <- chisq_sample |>
 
 chisq_distribution <- chisq_table |> 
   ggplot() +
+  stat_function(fun = dchisq, args = list(df = eval(parse(text = "chisq_table$df"))),
+                xlim = c(eval(parse(text = "chisq_table$chisq_statistic")), 30),
+                geom = "area", 
+                fill = "tomato", 
+                alpha = 0.5
+                ) +
   stat_function(fun = dchisq, args = list(df = eval(parse(text = "chisq_table$df"))), linewidth = 1, color = "blue") +
   geom_vline(aes(xintercept = chisq_statistic), color = "red", linetype = "dashed", linewidth = 1) +
   scale_x_continuous(limits = c(0, 30))
 
 
 
-bar_plot + stacked_bar_plot + chisq_distribution
+(bar_plot + stacked_bar_plot + chisq_distribution) + plot_layout(guides = "collect") + theme_bw()
 
 #Results
 
